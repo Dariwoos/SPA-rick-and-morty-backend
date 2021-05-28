6dc499d03df9efe415eaf6ab063863f970901e47
Dariwoos <tst.info20@gmail.com>
2021-05-28 18:08:07 +0000

    first comiit

--- src/models.py
+++ src/models.py
@@ -4,16 +4,19 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
+    bank_account = db.Column(db.String(120), unique=False, nullable=False)
+    loan = db.Column(db.String(80), unique=False, nullable=False)
+    dues = db.Column(db.String(80), unique=False, nullable=False)
+    payments = db.Column(db.String(80), unique=False, nullable=False)
+    balance = db.Column(db.String(80), unique=False, nullable=False)
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "bank_account": self.email,
+            "loan":self.loan,
+            "dues":self.dues,
+            "payments":self.payments,
+            "balance":self.balance
             # do not serialize the password, its a security breach
         }
